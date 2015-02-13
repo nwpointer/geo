8c5b25af9b984dc79aa233a78285bab82f9d5f6f
Intl Afairs Web Assistant <iawebassist@d39-15.uoregon.edu>
2015-02-13 15:58:48 -0800

    fields float left

--- sites/all/modules/program/index.js
+++ sites/all/modules/program/index.js
@@ -2,7 +2,7 @@
 
 //list.js initializations
 var options = { 
-  valueNames: [ 'discipline', 'country', 'term'] ,
+  valueNames: [ 'discipline', 'country', 'term', 'pricing', 'regions'] ,
   // item: '<li><a class="discipline"></a> <br /><span class="url"></span><p class="country"></p></li>'//,
   item: '<li><a class="url"><div class="img-wrap"><img class="image"></div><div class="content"><h3 class="title"></h3><p class="term"></p><p class="discipline"></p></div></a></li>'
 };
